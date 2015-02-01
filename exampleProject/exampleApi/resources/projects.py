e52f8a6dc172000f1c81020f10516af933151e5e
Viktoras Makauskas <viktoras@neglostyti.com>
2015-02-01 03:19:01 +0200

    more docs

--- exampleProject/exampleApi/resources/projects.py
+++ exampleProject/exampleApi/resources/projects.py
@@ -10,21 +10,38 @@ projectSchema = SchemaBuilder().object() \
         .build()
         
 def getList():
+    '''
+    this method will automatically bind to:
+        GET /projects/
+    '''
     pass
 
 @schema(projectSchema)
 def createItem(itemData):
     '''
     create a new project
+    
+    this method will automatically bind to:
+        POST /projects/
     '''
+    
+    
     return {'created':itemData}
 
 @schema(projectSchema)
 def editItem(itemId, itemData):
+    '''
+    this method will automatically bind to:
+        POST /projects/<itemId>
+    '''
     return {'edited':itemData}
 
 @example(projectSchema)
 def getItem(itemId):
+    '''
+    this method will automatically bind to:
+        GET /projects/<itemId>
+    '''
     return {'id':itemId,'title':'something'}
 
 
